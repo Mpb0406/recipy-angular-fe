bd21c6a11140714b7eae982b742c79a203ebbeef
Mike Bolloskis <mpb0406@gmail.com>
2022-08-26 21:32:41 -0400

    Added dataService and test method to make POST request to log user and added cors package to server.js

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private dataService: DataService) {}
   title = 'frontend';
+
+  logUser() {
+    this.dataService.logUser().subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
---
+++ frontend/src/app/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+  body = {
+    email: 'mpb0406@gmail.com',
+    password: '123456',
+  };
+
+  options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  logUser() {
+    console.log(this.body);
+    return this.http.post(
+      'http://localhost:5000/api/auth',
+      this.body,
+      this.options
+    );
+  }
+}
